1744a04684cc29fcf6cef15663f29f53fba4e44a
Gurpreet Singh <gurpreet.singh1@valuemomentum.com>
2025-05-02 13:30:59 +0530

    Created project with Start Library POM details

---
+++ src/main/java/com/vm/ec/starter_parent/StarterParentApplication.java
@@ -0,0 +1,13 @@
+package com.vm.ec.starter_parent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StarterParentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StarterParentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vm/ec/starter_parent/StarterParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vm.ec.starter_parent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterParentApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
